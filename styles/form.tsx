40c167d363e82f4cae11b16d6947a8b77dadc350
Marek Sommer <marek.sommer.1@gmail.com>
2023-05-21 00:01:31 +0200

    Add form styles

---
+++ styles/form.tsx
@@ -0,0 +1,8 @@
+export default {
+  label: 'block text-gray-700 text-sm font-bold mb-2',
+  input: `shadow border rounded appearance-none w-full text-gray-700
+          py-2 px-3 leading-tight 
+          focus:outline-none focus:shadow-outline`,
+  inputError: `border-red-500`,
+  errorMessage: `text-red-500 text-xs italic`
+}
