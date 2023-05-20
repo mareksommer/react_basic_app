e8868365e67c0f26412daea2db28b094d16c72ae
Marek Sommer <marek.sommer.1@gmail.com>
2023-05-21 00:01:42 +0200

    Add buttons styles

---
+++ styles/buttons.tsx
@@ -0,0 +1,4 @@
+export default {
+  primary: `bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`,
+  primaryLink: `inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800`
+}
